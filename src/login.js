547e209704a2c7088224de4b08bb28dccd9597f2
Mikael Mello <mikaelmmello@gmail.com>
2019-01-08 15:54:29 -0200

    project: add linter based on airbnb style

--- src/login.js
+++ src/login.js
@@ -1,22 +1,22 @@
-const loginEmail = async function(credentials) {
-    
+const loginEmail = async function loginEmail(/* credentials */) {
+  throw new Error('Not implemented');
 };
 
-const loginFacebook = async function(credentials) {
-    
+const loginFacebook = async function loginFacebook(/* credentials */) {
+  throw new Error('Not implemented');
 };
 
-const loginGoogle = async function(credentials) {
-    
+const loginGoogle = async function loginGoogle(/* credentials */) {
+  throw new Error('Not implemented');
 };
 
-const loginTwitter = async function(credentials) {
-    
+const loginTwitter = async function loginTwitter(/* credentials */) {
+  throw new Error('Not implemented');
 };
 
 module.exports = {
-    email: loginEmail,
-    facebook: loginFacebook,
-    google: loginGoogle,
-    twitter: loginTwitter,
+  email: loginEmail,
+  facebook: loginFacebook,
+  google: loginGoogle,
+  twitter: loginTwitter,
 };
--- src/ticktick.js
+++ src/ticktick.js
@@ -2,28 +2,34 @@ const rp = require('request-promise');
 const login = require('./login');
 
 function TickTick() {
-    this.request = rp.defaults({
-        jar: true,
-    });
-    this.baseUri = 'https://api.ticktick.com/api/v2';
+  this.request = rp.defaults({
+    jar: true,
+  });
+  this.baseUri = 'https://api.ticktick.com/api/v2';
 }
 
-TickTick.prototype.login = async function(options) {    
-    if (!options) {
-        throw new Error('No options provided for login');
-    }
+TickTick.prototype.login = async function _login(options) {
+  if (!options) {
+    throw new Error('No options provided for login');
+  }
 
-    if (options.email) {
-        await login.email.call(this, options.email);
-    } else if (options.google) {
-        await login.google.call(this, options.google);
-    } else if (options.facebook) {
-        await login.facebook.call(this, options.facebook);
-    } else if (options.twitter) {
-        await login.twitter.call(this, options.twitter);
-    } else {
-        throw new Error('No options provided for login');
-    }
-}
+  if (options.email) {
+    return login.email.call(this, options.email);
+  }
 
-module.exports = exports = new TickTick();
+  if (options.google) {
+    return login.google.call(this, options.google);
+  }
+
+  if (options.facebook) {
+    return login.facebook.call(this, options.facebook);
+  }
+
+  if (options.twitter) {
+    return login.twitter.call(this, options.twitter);
+  }
+
+  throw new Error('No options provided for login');
+};
+
+module.exports = new TickTick();
