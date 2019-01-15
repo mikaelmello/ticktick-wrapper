1d5742b02f69a5be2d14138436805623fdbb35f6
Mikael Mello <mikaelmmello@gmail.com>
2019-01-15 16:47:58 -0200

    docs: reminder: add descriptions

--- src/reminder.js
+++ src/reminder.js
@@ -1,23 +1,53 @@
 const ObjectID = require('bson-objectid');
 
+/**
+ * Error thrown when a call to create a {@link Reminder} is made
+ * with an invalid time unit parameter.
+ */
 class InvalidUnitError extends Error {
   constructor(unit) {
     super(`The unit "${unit}" is invalid`);
   }
 }
 
+/**
+ * Class that represents a Reminder used in {@link Task}s.
+ * @class
+ * @constructor
+ * @param {Object} properties - Properties of the {@link Reminder}.
+ * @param {string=} properties.id - Object ID of the Reminder, only defined when this is an
+ * instantiation of a pre-existing reminger. On new reminders this must be empty in order
+ * to generate a new ObjectID
+ * @param {string=} properties.trigger - Custom trigger in the pre-defined format
+ * /^TRIGGER:-?PT[0-9]+[SMHD]$/
+ */
 function Reminder(properties) {
   this.id = properties.id || ObjectID();
   this.trigger = properties.trigger || 'TRIGGER:PT0S';
 }
 
+/**
+ * Enum for the time unit used in the creation of a reminder
+ * @readonly
+ * @enum {Number}
+ */
 Reminder.TimeUnit = {
+  /** Used when the reminder is set in seconds */
   SECONDS: 0,
+  /** Used when the reminder is set in minutes */
   MINUTES: 1,
+  /** Used when the reminder is set in hours */
   HOURS: 2,
+  /** Used when the reminder is set in days */
   DAYS: 3,
 };
 
+/**
+  * Creates a reminder object to be used when creating a {@link Task}. The reminder
+  * will go off at {quantity} {units} before the date set for the {@link Task}.
+  * @param {Number} quantity - Quantity of unit.
+  * @param {Reminder.TimeUnit} unit - Unit of time
+  */
 Reminder._create = function _create(quantity, unit) {
   let trigger;
 
